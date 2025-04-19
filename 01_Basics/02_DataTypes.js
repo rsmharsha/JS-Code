9fe18042fea0f7b32cc21d2951a3a91930d147c7
star-lord <143583455+rsmharsha@users.noreply.github.com>
2025-04-19 00:47:59 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treats all JS code as newer version
+
+// alert("Hello"); // we are using node.js not browser
+
+
+// code readibility is priority
+console.log(3+3);  
+console.log("harsha")
+
+// Documentation for JS - mdn, tc39(ecma)
+
+let name = "harsha"
+let age = 24
+let isLoggedIn = true
+
+//  - primitive datatypes -
+// number
+// BigInt
+// string => ""
+// boolean
+// null => standalone value
+// let state = null; we can assign null value to variable
+// undefined
+// symbol - unique
+
+// object
+console.log(typeof "harsha")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
