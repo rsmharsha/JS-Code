11e9abea573430ed5baf0a4b69f9a9582a8e2521
star-lord <143583455+rsmharsha@users.noreply.github.com>
2025-04-20 12:46:18 +0000

    datatype-summary

---
+++ 01_Basics/05_dataTypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive Type
+// 7 types - String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// JS is dynamically typed languange.
+const score = 1000;
+const scoreVal = 99.67;
+
+const isLoggedIn = false;
+const outsideTemp = null; // type => object
+let userEmail;
+
+const id = Symbol('123')
+
+const bigNumber = 34556736356357666n;
+
+
+
+
+// Reference Type (Non primitive)
+// 3 types - Array, Objects, functions
+
+const avengers = ["Ironman", "Thor", "Loki", "Captain America"]
+
+const myObj = {
+    name:"Tony Stark",
+    address:"Malibu Point, California",
+    spouse:"Pepper Potts"
+}
+
+let myFunc = function (){
+    console.log("Iam Ironman");
+}
+
